797b44717b402ceebc0139a684c391fc0a9a6828
JosiasAssamoi <assamoi.josias@gmail.com>
2019-04-15 02:41:18 +0200

    GUI initialized

---
+++ InscriptionsSportives/src/gui/Main.java
@@ -0,0 +1,27 @@
+package gui;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent parent = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(parent);
+        primaryStage.setTitle("Inscriptions Sportives");
+        primaryStage.setScene(scene);
+     //   scene.getStylesheets().add(getClass().getResource("main.css").toString());
+        primaryStage.show();
+    }
+
+	
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+}
